f760994c6193f7431c4507cc776c1c107ef9a5b5
Good-gulf <good-gulf@wp.pl>
2020-07-18 18:18:46 +0200

    prawie koniec - chyba

--- power-test.py
+++ power-test.py
@@ -1,4 +1,32 @@
 import unittest
-from my-power import *
+from my_power import *
 
-def my-power-u
+
+class TestPower(unittest.TestCase):
+    def setUp(self) -> None:
+        self.basis = 2
+        self.exponent = 2
+
+    def test_basic(self):
+        result = pow(self.basis, self.exponent)
+        self.assertEqual(result,my_power(self.basis, self.exponent))
+
+    def test_negative(self):
+        self.basis = (-2)
+        result = pow(self.basis, self.exponent)
+        self.assertEqual(result,my_power(self.basis, self.exponent))
+
+    def test_fractions(self):
+        self.basis = 0.5
+        result = pow(self.basis, self.exponent)
+        self.assertEqual(result,my_power(self.basis, self.exponent))
+
+    def test_fractions2(self):
+        self.basis = 2
+        self.exponent = 0.5
+        result = pow(self.basis, self.exponent)
+        self.assertEqual(result,my_power(self.basis, self.exponent))
+
+
+if __name__ == '__main__':
+    unittest.main()
